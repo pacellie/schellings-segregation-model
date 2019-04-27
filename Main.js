87cca9ab333705add70c5d778b9983dc1ffd490b
Martin Rau <martin.rau@tum.de>
2019-04-27 23:53:48 +0200

    add preferences settings + styling

--- Main.js
+++ Main.js
@@ -4,21 +4,30 @@ import { initChart, updateChart, clearChart } from './Plotter';
 import { segregation, isolation, density } from './Statistic';
 
 function main() {
-  const ctx      = document.getElementById('simulation-canvas').getContext('2d');
+  const ctx = document.getElementById('simulation-canvas').getContext('2d');
+
   const size     = document.getElementById('generate-size');
+  const radius   = document.getElementById('generate-neighborhood-radius');
   const squares  = document.getElementById('generate-mixture-squares');
   const circles  = document.getElementById('generate-mixture-circles');
   const generate = document.getElementById('generate-button');
+
+  const preferences  = document.getElementById('preferences-button');
+  const squaresLower = document.getElementById('preferences-squares-lower');
+  const squaresUpper = document.getElementById('preferences-squares-upper');
+  const circlesLower = document.getElementById('preferences-circles-lower');
+  const circlesUpper = document.getElementById('preferences-circles-upper');
+
   const simulate = document.getElementById('simulate-button');
 
   const config = {
-    neighborhoodRadius: 1,
-    percentageA       : 0.33,
-    percentageB       : 0.33,
-    lowerBoundA       : 0.50,
-    lowerBoundB       : 0.50,
-    upperBoundA       : 1.00,
-    upperBoundB       : 1.00,
+    neighborhoodRadius: Number(radius.value),
+    percentageA       : Number(squares.value) / 100,
+    percentageB       : Number(circles.value) / 100,
+    lowerBoundA       : Number(squaresLower.value) / 100,
+    lowerBoundB       : Number(circlesLower.value) / 100,
+    upperBoundA       : Number(squaresUpper.value) / 100,
+    upperBoundB       : Number(circlesUpper.value) / 100,
   };
 
   let model = new Model(30, config);
@@ -31,6 +40,7 @@ function main() {
 
   const run = () => {
     clear(ctx);
+
     const stepped = model.simulate();
     if (!stepped) {
       simulate.value = 'Simulate';
@@ -41,6 +51,7 @@ function main() {
       updateChart('isolation', isolation(model));
       updateChart('density', density(model));
     }
+
     render(ctx, model);
   };
 
@@ -55,10 +66,22 @@ function main() {
     initChart('isolation', 'Isolation');
     initChart('density', 'Density');
 
+    config.neighborhoodRadius = Number(radius.value);
     config.percentageA = Number(squares.value) / 100;
     config.percentageB = Number(circles.value) / 100;
 
     model = new Model(Number(size.value), config);
+    render(ctx, model);
+  };
+
+  preferences.onclick = (_) => {
+    clear(ctx);
+
+    config.lowerBoundA = Number(squaresLower.value) / 100;
+    config.lowerBoundB = Number(circlesLower.value) / 100;
+    config.upperBoundA = Number(squaresUpper.value) / 100;
+    config.upperBoundB = Number(circlesUpper.value) / 100;
+
     render(ctx, model);
   };
 
